fef6cfc8a4c8cea700cfb2339607bed1e923fdf4
Your Name <you@example.com>
2025-06-26 21:11:17 +0300

    first commit

---
+++ src/main/java/com/medoptica/medoptica/MedopticaApplication.java
@@ -0,0 +1,13 @@
+package com.medoptica.medoptica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedopticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedopticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medoptica/medoptica/MedopticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medoptica.medoptica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedopticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
