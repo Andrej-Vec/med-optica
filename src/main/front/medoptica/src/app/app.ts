9cbd218533d50b4a5c9e680d55a802beeddc1bad
Your Name <you@example.com>
2025-06-28 21:21:59 +0300

    add slider html

--- src/main/front/medoptica/src/app/app.ts
+++ src/main/front/medoptica/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './header/header.component';
+import {SliderComponent} from './slider/slider.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SliderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/main/front/medoptica/src/app/slider/slider.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-slider',
+  imports: [],
+  templateUrl: './slider.component.html',
+  styleUrl: './slider.component.scss',
+  standalone: true
+})
+export class SliderComponent {
+
+}
