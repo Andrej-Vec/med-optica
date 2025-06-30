750e3c05e5f8f7e4e33b76b26b00781834ae1c46
Your Name <you@example.com>
2025-06-30 21:13:05 +0300

    add component products

--- src/main/front/medoptica/src/app/app.ts
+++ src/main/front/medoptica/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from './header/header.component';
-import {SliderComponent} from './slider/slider.component';
+import {HeaderComponent} from './components/header/header.component';
+import {SliderComponent} from './components/slider/slider.component';
+import {ProductsComponent} from './components/products/products.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SliderComponent],
+  imports: [HeaderComponent, SliderComponent, ProductsComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
